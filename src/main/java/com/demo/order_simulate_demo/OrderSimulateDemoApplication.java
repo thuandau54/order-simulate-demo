ec6207d31d6d66d9ec61eac547d3d55fce048615
thuandau54 <thuandau54@gmail.com>
2025-07-26 22:59:03 +0700

    add

---
+++ src/main/java/com/demo/order_simulate_demo/OrderSimulateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.order_simulate_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSimulateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSimulateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/order_simulate_demo/OrderSimulateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.order_simulate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSimulateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
